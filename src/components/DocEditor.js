7d131ce2b82848e0f9f9ac0b073f82736fb851fe
github-usman <usmantoyou@gmail.com>
2024-01-26 20:42:41 +0530

    editor page completed

--- src/components/DocEditor.js
+++ src/components/DocEditor.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; // Import the styles
+import React, { useState } from 'react';
 
 const DocEditor = () => {
+
+  const [content, setContent] = useState('');
+
+  const handleChange = (value) => {
+    setContent(value);
+  };
+
   return (
-    <div style={{height:"100%",width:"100%",backgroundColor:"blue"}} >
-        DocEditor
+        <div className='main-editor'>
+            <div className='writable-area'>
+
+        <ReactQuill value={content} onChange={handleChange} />
+            </div>
         </div>
-  )
-}
+  );
+};
+
 
 export default DocEditor
